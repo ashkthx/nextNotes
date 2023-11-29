c8b133d8e38c5f19d3447edd8fadf622408bc2e7
ash <ashleymwilloughby@gmail.com>
2023-11-29 09:12:13 -0600

    initial commit

---
+++ nextNotes/next.config.js
@@ -0,0 +1,14 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {}
+
+// module.exports = nextConfig
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
+    swcMinify: true,
+    experimental: { appDir: true },
+    reactStrictMode: false,
+}
+
+module.exports = nextConfig
